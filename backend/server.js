7c00b7c8c7439230009fe1bd766ed416864601bd
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-03-13 17:44:45 +0530

    04_MONGODB_connected

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import authRoutes from "./routes/auth.route.js";
 import { ENV_VARS } from './config/envVars.js';
 
+import { connectDB } from './config/db.js';
+
 const app = express();
 const PORT = ENV_VARS.PORT;
 
@@ -12,4 +14,7 @@ app.use("/api/v1/auth", authRoutes);
 
 app.listen( PORT,()=>{
   console.log("Server is running at http://localhost:",+PORT);
+  connectDB();
+
+
 });
