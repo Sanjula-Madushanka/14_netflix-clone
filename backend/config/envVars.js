ddc2166464479b813aaa76896cd152264330976f
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-03-15 00:02:50 +0530

    Updated

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -6,5 +6,5 @@ export const ENV_VARS = {
     PORT: process.env.PORT || 5000,
     MONGO_URI: process.env.MONGO_URI,
    JWT_SECRET: process.env.JWT_SECRET,
-
+   TMDB_API_KEY: process.env.TMDB_API_KEY
 };
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
-import express from 'express';
-import authRoutes from "./routes/auth.route.js";
-import { ENV_VARS } from './config/envVars.js';
+import express from "express";
 
-import { connectDB } from './config/db.js';
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); 
 
-
-
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
-app.listen( PORT,()=>{
- console.log(`Server is running at http://localhost:${PORT}`);
-
-  connectDB();
-
-
+app.listen(PORT, () => {
+	console.log("Server started at http://localhost:" + PORT);
+	connectDB();
 });
+
+
+
