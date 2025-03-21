4519e3c97a71cacf49f13d80ba166dc723fbcc33
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-03-21 13:28:49 +0530

    08_fix the logout but still working issue(Protect Route)

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,33 +1,28 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-
-username: {
-    type: String,
-    required: true,
-    unique: true,
-},
-email: {
-    type: String,
-    required: true,
-    unique: true,
-},
-password: {
-    type: String,
-    required: true,
-},
-image: {
-    type: String,
-   default:""
-},
-searchHistory: {
-    type: [String],
-    default: [],
-}
-
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+		default: "",
+	},
+	searchHistory: {
+		type: Array,
+		default: [],
+	},
 });
 
-const User =mongoose.model("User", userSchema);
-//users
-
-export default User
+export const User = mongoose.model("User", userSchema);
