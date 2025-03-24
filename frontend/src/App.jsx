1eefb1592034d2cb8293ded9fe4f9e9a8018dfe7
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-03-24 05:43:16 +0530

    11_Create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
+import {  Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+
 
 function App() {
-
-
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
