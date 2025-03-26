b890fda14eeeba591831b943293d3f431c0094a6
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-03-26 10:37:10 +0530

    15_Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+        </Routes>
+        
+      
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
