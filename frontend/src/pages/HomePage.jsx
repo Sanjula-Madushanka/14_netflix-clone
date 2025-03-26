3057eefe243e26065d923c34b96b581d51718326
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-03-26 09:39:02 +0530

    14_AuthScreen

--- frontend/src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomePage = () => {
-  return (
-   <div className='hero-bg h-screen'>Homepage</div>
-  )
-}
-
-export default HomePage
---
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <div>
+      homescreen
+    </div>
+  )
+}
+
+export default HomeScreen
