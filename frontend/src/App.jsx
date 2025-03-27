fff45b558bdb3a562c47cefe650041fd2499a2be
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-03-27 09:26:02 +0530

    (18)_divided homescreen and authscreen perfectly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import Footer from "./components/Footer.jsx";
 import { Toaster } from "react-hot-toast";
-import { useAuthStore } from "./pages/store/authUser.jsx";
+import { useAuthStore } from "./pages/store/authUser.js";
 import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -14,6 +15,16 @@ function App() {
 		authCheck();
 	}, [authCheck]);
 
+	if (isCheckingAuth) {
+		return (
+			<div className='h-screen'>
+				<div className='flex justify-center items-center bg-black h-full'>
+					<Loader className='animate-spin text-red-600 size-10' />
+				</div>
+			</div>
+		);
+	}
+
 
   return (
     
@@ -21,8 +32,8 @@ function App() {
       <div>
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/login' element={!user ?<LoginPage />: <Navigate to ={"/"}/>} />
+          <Route path='/signup' element={!user ?<SignUpPage />: <Navigate to ={"/"}/>} />
         </Routes>
       
         <Footer />
--- frontend/src/pages/store/authUser.js
+++ frontend/src/pages/store/authUser.js
@@ -8,6 +8,8 @@ export const useAuthStore = create((set) => ({
 	isSigningUp: false,
 	isCheckingAuth: true,
 	isCheckingAuth: true,
+	isLoggingOut: false,
+	isLoggingIn: false,
 	signup: async (credentials) => {
 		set({ isSigningUp: true });
 		try {
@@ -19,6 +21,31 @@ export const useAuthStore = create((set) => ({
 			set({ isSigningUp: false, user: null });
 		}
 	},
+
+	login: async (credentials) => {
+		set({ isLoggingIn: true });
+		try {
+			const response = await axios.post("/api/v1/auth/login", credentials);
+			set({ user: response.data.user, isLoggingIn: false });
+		} catch (error) {
+			set({ isLoggingIn: false, user: null });
+			toast.error(error.response.data.message || "Login failed");
+		}
+	},
+	logout: async () => {
+		set({ isLoggingOut: true });
+		try {
+			await axios.post("/api/v1/auth/logout");
+			set({ user: null, isLoggingOut: false });
+			toast.success("Logged out successfully");
+		} catch (error) {
+			set({ isLoggingOut: false });
+			toast.error(error.response.data.message || "Logout failed");
+		}
+	},
+
+
+
 	authCheck: async () => {
 		set({ isCheckingAuth: true });
 		try {
