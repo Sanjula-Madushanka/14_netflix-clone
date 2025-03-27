54e80a7fab4def4dac5f52ba7b17f51b08e05da1
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-03-27 22:16:33 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ import { protectRoute } from "./middleware/protectRoute.js";
 const app = express();
 const PORT = ENV_VARS.PORT;
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
