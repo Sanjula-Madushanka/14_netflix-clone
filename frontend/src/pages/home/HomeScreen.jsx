5b9181e16942a1b3aa3c3bc22e5d0de6d0cec722
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-04-01 12:08:53 +0530

    20_complete_homepage_come to the page trending movie.

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -3,15 +3,28 @@ import Navbar from '../../components/Navbar';
 import { Link } from 'react-router-dom';
 import { Info, Play } from 'lucide-react';
 import useGetTrendingContent from '../../hooks/useGetTrendingContent';
+import { ORIGINAL_IMG_BASE_URL } from '../../utils/constants';
 
 const HomeScreen = () => {
 const {trendingContent}=useGetTrendingContent();
 console.log("trendingContent", trendingContent);
+
+//TODO add a loading spinner
+if (!trendingContent)
+  return (
+    <div className='h-screen text-white relative'>
+      <Navbar />
+      <div className='absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer' />
+    </div>
+  );
+
+
   return (
   <>
   <div className='relative h-screen text-white '>
     <Navbar/>
-    <img src='/extraction.jpg' alt='Hero img'
+    <img src={ORIGINAL_IMG_BASE_URL+ trendingContent?.backdrop_path}
+     alt='Hero img'
     className='absolute top-0 left-0 w-full h-full object-cover -z-50'
     />
     <div className='absolute top-0 left-0 w-full h-full bg-black/50 -z-50'
@@ -27,16 +40,26 @@ console.log("trendingContent", trendingContent);
   <div className='max-w-2xl'>
   
 							
-  <h1 className='mt-4 text-6xl font-extrabold text-balance'>Extraction</h1>
-  <p className='mt-4 text-lg'>2014 | 18+</p>
+  <h1 className='mt-4 text-6xl font-extrabold text-balance'>
+      {trendingContent?.title || trendingContent?.name}
+    </h1>
+  <p className='mt-2 text-lg'>
+              {trendingContent?.release_date?.split("-")[0] ||
+								trendingContent?.first_air_date.split("-")[0]}{" "}
+							| {trendingContent?.adult ? "18+" : "PG-13"}
+
+  </p>
 						
-				<p className=''>
-          Chris hemsworth stars in this nonstaop action-thriller with Rudharksh Jaiswal , Randeep Hooda and Golshifteh Farhani.
-        </p>
+  <p className='mt-4 text-lg'>
+							{trendingContent?.overview.length > 200
+								? trendingContent?.overview.slice(0, 200) + "..."
+								: trendingContent?.overview}
+						</p>
       </div>
       <div className='flex mt-8'>
 
-       <Link to="/watch/123"
+       <Link 
+      to={`/watch/${trendingContent?.id}`}
       className='bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex
 							 items-center'
        >
@@ -44,7 +67,8 @@ console.log("trendingContent", trendingContent);
        Play
        </Link>
 
-       <Link to="/watch/123"
+       <Link 
+     to={`/watch/${trendingContent?.id}`}
       className='bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center'
       >
        <Info className='size-6  mr-2'/>
